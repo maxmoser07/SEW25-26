f5033ca62801381ff8f5f9876ee73875f2dc40b4
maxmoser07 <m.moser@htlkrems.at>
2025-09-04 08:44:03 +0200

    test commit

--- MultithreadingExample/MultithreadingExample/obj/Debug/net9.0/MultithreadingExample.AssemblyInfo.cs
+++ MultithreadingExample/MultithreadingExample/obj/Debug/net9.0/MultithreadingExample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultithreadingExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e43261a49877a86b25bfde914ffaeb05ea4bf4d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultithreadingExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultithreadingExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
