3b0899fdce54c62204032334f24c6909784d5c73
max <m.moser@htlkrems.at>
2026-02-12 10:16:43 +0100

    Commit

---
+++ APIUebung/DemoConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Model.Entities;
+
+using (MyDbContext context = new MyDbContext())
+{
+    foreach (var d in context.Demos)
+    {
+        Console.WriteLine($"{d.Id}/{d.Value}");
+    }
+}
---
+++ APIUebung/Model/Context/MyDbContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Model.Entities;
+
+public partial class MyDbContext : DbContext
+{
+    public MyDbContext()
+    {
+    }
+
+    public MyDbContext(DbContextOptions<MyDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Demo> Demos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySQL("Server=localhost;Port=3306;Database=demo;User=root;Password=insy;   ");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Demo>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("demo");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Value)
+                .HasMaxLength(25)
+                .HasColumnName("value");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
