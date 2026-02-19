8aadbb3ef17b421e4f0a7e37dcfe4e58e81d95c3
max <m.moser@htlkrems.at>
2026-02-19 09:20:00 +0100

    project changes 3

--- APIUebung/BlazorWebAssembly/obj/Debug/net10.0/BlazorWebAssembly.AssemblyInfo.cs
+++ APIUebung/BlazorWebAssembly/obj/Debug/net10.0/BlazorWebAssembly.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorWebAssembly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f73a35c829d973ad48908d30bc1de3840de7b152")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6450f251bc98e549241a05f493353a4689cb80f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorWebAssembly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorWebAssembly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
