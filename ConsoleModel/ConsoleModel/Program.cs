bcebbb5562e3b5eb86aae4b5299225c7cd940fc5
max <m.moser@htlkrems.at>
2026-02-23 15:30:00 +0100

    project changes 5

---
+++ ConsoleModel/ConsoleModel/Program.cs
@@ -0,0 +1,10 @@
+﻿using ConsoleModel;
+
+Blog sew =new Blog {Id = 1, Topic = "Sew"};
+Post p = new Post {Id = 1, BlogId = 1, Text = "Suppa"};
+
+p.Blog = sew;
+Console.WriteLine(p.Blog.Topic);
+
+Console.WriteLine(sew.Posts[0].Text);
+sew.Posts.Add(p);
